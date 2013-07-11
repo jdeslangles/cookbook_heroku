1d245be8c1858970af2fc6593233d1f40b7370a6
jdeslangles <julien.deslangles@gmail.com>
2013-07-11 16:50:58 +0100

    Background image style added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def define_background
+    if @recipe
+     @recipe.recipe_image.url
+    elsif @ingredient
+     @ingredient.ingredient_image.url
+    else
+      raw "'/assets/leather.png'"
+     end
+  end
 end
+
+
+# background-image: url(<%= define_background %>); background-size: cover;
