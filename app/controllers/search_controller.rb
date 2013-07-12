0ad4a52e37ccca8177cb96924b921f027f8df7c5
jdeslangles <julien.deslangles@gmail.com>
2013-07-12 14:26:48 +0100

    Full search functionality added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,9 @@ class SearchController < ApplicationController
     @q = Recipe.search(params[:q])
     @recipes = @q.result(distinct: true) # 'distinct' will only get one instance of each article if they match my search
     if @q.result.empty?
-      flash[:error] = "No matches for #{params[:q]["title_cont"]} - sorry :("
+      flash[:error] = "No matches for #{params[:q]["title_or_ingredients_name_or_categories_category_name_or_author_cont"]} - sorry :("
+      # flash[:error] = "No matches for #{params[:q]["title_cont"]} - sorry :("
+
     end
   end
 
